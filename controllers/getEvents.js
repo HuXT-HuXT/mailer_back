5226f6c73cfb77e9c408a6c413d38bb9be5b0d3e
Win11 <huxt-huxt@yandex.ru>
2023-08-24 19:54:21 +0300

    working on it

--- controllers/getEvents.js
+++ controllers/getEvents.js
@@ -1,2 +1,24 @@
 import DataFetcher from '../classes/dataFetcher.js';
+import EventLetter from '..//classes/letter.js';
 
+const eventApi = new DataFetcher(
+  process.env.STRAPI_BASE_URL,
+  process.env.STRAPI_TOKEN
+);
+
+async function getLetters() {
+  const events = await eventApi.getEvents();
+  const eventLetters = events.map(event => new EventLetter(event));
+  return eventLetters;
+};
+
+async function getCurrentLetter(uuid) {
+  const event = await eventApi.getEvent(uuid);
+  const eventLetter = new EventLetter(event);
+  return eventLetter;
+};
+
+export {
+  getLetters,
+  getCurrentLetter
+};
